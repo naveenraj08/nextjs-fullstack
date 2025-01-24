f9ca4974560cb578351c33052f723be79d83f44f
NaveenRaj K <naveenraj.k@ewallsolutions.com>
2025-01-24 12:39:03 +0530

    Integrate markdown editor on sanity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -11,6 +11,7 @@ import { structureTool } from 'sanity/structure'
 import { apiVersion, dataset, projectId } from './sanity/env'
 import { schema } from './sanity/schemaTypes'
 import { structure } from './sanity/structure'
+import {markdownSchema} from 'sanity-plugin-markdown'
 
 export default defineConfig({
   basePath: '/studio',
@@ -23,5 +24,7 @@ export default defineConfig({
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
+
+    markdownSchema()
   ],
 })
--- sanity/schemaTypes/startup.ts
+++ sanity/schemaTypes/startup.ts
@@ -42,7 +42,7 @@ export const startup = defineType({
         }),
         defineField({
             name: "pitch",
-            type: "text",
+            type: "markdown",
         }),
     ]
 });
