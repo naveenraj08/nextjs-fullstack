b7ff309d5d6d9bb8565bd22974d9ed387f0092a7
Naveen <rajnaveen677@gmail.com>
2025-02-21 00:35:51 +0530

    Create server and client component for sidebar

---
+++ app/components/RecentPosts.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { formateDate } from "../lib/utils";
+
+export const RecentPosts = ({ post }) => {
+  const { _id, title, image, _createdAt } = post;
+  return (
+    <li>
+      <Link
+        href={`/startup/${_id}`}
+        className="p-3 rounded-md flex items-center gap-4 w-full text-sm transition duration-200 hover:bg-gray-100"
+      >
+        <span>
+          <Image
+            src={image}
+            alt={title}
+            className="w-12 h-12 rounded-md max-w-[48px] max-h-[48px] object-cover object-center"
+            width="40"
+            height="48"
+          />
+        </span>
+        <span className="block">
+          {title}
+          <span className="text-xs inline-flex items-center gap-1 mt-2 text-[#7D838F] w-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-4 relative bottom-[1px]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
+              />
+            </svg>
+            {formateDate(_createdAt)}
+          </span>
+        </span>
+      </Link>
+    </li>
+  );
+};
--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,7 +1,39 @@
+import { client } from "@/sanity/lib/client";
+import { GET_RECENT_POST } from "@/sanity/lib/queries";
 import React from "react";
+import { RecentPosts } from "./RecentPosts";
+import Link from "next/link";
 
-const SideBar = () => {
-  return <div className="p-5">SideBar</div>;
+const SideBar = async () => {
+  const fromTimeStampInMilliSec = new Date(
+    Date.now() - 7 * 24 * 60 * 60 * 1000
+  );
+  const fromTimeStamp = fromTimeStampInMilliSec.toISOString();
+
+  const posts = await client.fetch(GET_RECENT_POST, { fromTimeStamp });
+
+  return (
+    <>
+      <div className="text-lg text-blue-600 font-semibold p-5 pb-0">
+        Recent Posts
+      </div>
+      <ul className="block p-2">
+        {posts.length > 0 ? (
+          posts.map((post: any) => <RecentPosts post={post} key={post?._id} />)
+        ) : (
+          <li className="text-sm font-medium">
+            Sorry! No Post for this week? how about creating one
+            <Link
+              href={"/startup/create"}
+              className="relative inline-block rounded-lg bg-blue-500 text-white px-5 py-2 text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              Create Post
+            </Link>
+          </li>
+        )}
+      </ul>
+    </>
+  );
 };
 
 export default SideBar;
