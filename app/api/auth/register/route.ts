0569aa763a87af610e41bbc0a392e300ccdd2c95
naveenraj08 <rajnaveen677@gmail.com>
2025-08-12 23:59:55 +0530

    Register a new user

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -8,6 +8,7 @@ export async function POST(req: NextRequest) {
     try {
         const userData = await req.json();
         const { name, email, password } = userData;
+
         const hash = await bcrypt.hash(password, 10);
         const existingUser = await client.withConfig({ useCdn: false }).fetch(AUTHOR_BY_EMAIL, { email });
 
@@ -17,6 +18,7 @@ export async function POST(req: NextRequest) {
                 _id: `author-${Date.now().toString(36)}`,
                 id: Date.now().toString() + Math.floor(Math.random() * 1000).toString(),
                 name,
+                username: name,
                 email,
                 password: hash
             });
