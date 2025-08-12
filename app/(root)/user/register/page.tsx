5f12cf84fbbe7aa39e33f7415803a81b63b8221a
naveenraj08 <rajnaveen677@gmail.com>
2025-08-13 00:00:22 +0530

    Page spacing update

--- app/(root)/user/register/page.tsx
+++ app/(root)/user/register/page.tsx
@@ -3,7 +3,7 @@ import UserResgistration from "@/app/components/UserResgistration";
 const page = async () => {
 
     return (
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 py-10 lg:px-8 flex flex-col-reverse lg:flex-row justify-start items-start gap-5">
+        <div className="mx-auto max-w-7xl px-5 sm:px-6 py-10 lg:px-8">
             <main className="font-work-sans flex-1">
                 <UserResgistration />
             </main>
